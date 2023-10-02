c94097f671b0c2cfc0d848f533805a579279d7d8
Yohann Ciurlik <yohann.ciurlik@capgemini.com>
2023-10-02 18:12:52 +0200

    feat: update dependencies versions

--- cli.js
+++ cli.js
@@ -6,22 +6,30 @@
  */
 const express = require('express')
 const Queue = require('bull')
-const QueueMQ = require('bullmq')
-const { createBullBoard } = require('bull-board')
-const { BullAdapter } = require('bull-board/bullAdapter')
+//const Queue = require('bullmq')
+const { createBullBoard } = require('@bull-board/api')
+const { BullAdapter } = require('@bull-board/api/bullAdapter')
+//const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter');
+const { ExpressAdapter } = require('@bull-board/express');
 
 const queueName = process.env.BULL_QUEUE || 'messages'
 const queueCnxString = process.env.BULL_CONNECTION || 'redis://127.0.0.1:6379'
-const messagesQueue = new Queue(queueName, queueCnxString);
+const messagesQueue = new Queue(queueName, { connection: {
+    queueCnxString
+  }});
 
-const { router, setQueues, replaceQueues } = createBullBoard([
-    new BullAdapter(messagesQueue)
-])
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/admin/queues');
+
+const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
+    queues: [new BullAdapter(messagesQueue)],
+    serverAdapter: serverAdapter,
+  });
 
 const app = express()
 const port = process.env.BULL_PORT || 3000
 
-app.use('/', router)
+app.use('/admin/queues', serverAdapter.getRouter());
 
 app.listen(port, () => {
     console.log(`Bull dashboard available at http://localhost:${port}`)
